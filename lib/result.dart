0a8f3a04057382b2652247e372fc9a81ea738c5c
Mitsutan <83488293+Mitsutan@users.noreply.github.com>
2024-03-14 17:45:16 +0900

    複数検出対応

--- lib/result.dart
+++ lib/result.dart
@@ -10,55 +10,60 @@ class ScanDataWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // コードから読み取った文字列
-    String codeValue = scandata?.barcodes.first.rawValue ?? 'null';
-    // コードのタイプを示すオブジェクト
-    BarcodeType? codeType = scandata?.barcodes.first.type;
-    // コードのタイプを文字列にする
-    String cardTitle = "[${'$codeType'.split('.').last}]";
-    // 読み取った内容を表示するウィジェット
-    dynamic cardSubtitle = Text(codeValue,
-        style: const TextStyle(fontSize: 23, color: Color(0xFF553311)));
-
-    // タイプがURLである場合
-    if (codeType == BarcodeType.url) {
-      cardTitle = 'どこかのURL';
-      cardSubtitle = InkWell(
-        child: Text(
-          codeValue,
-          style: const TextStyle(
-            fontSize: 23,
-            color: Color(0xFF1133DD), // 藍色の文字
-            decoration: TextDecoration.underline, // 下線
-            decorationColor: Color(0xFF1133DD), // 下線の色
-          ),
-        ),
-        // 押したらウェブサイトに入る
-        // onTap: () async {
-        //   if (await canLaunchUrlString(codeValue)) {
-        //     await launchUrlString(codeValue);
-        //   }
-        // },
-      );
-    }
-
     return Scaffold(
       appBar: AppBar(
-        backgroundColor:
-            Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('スキャンの結果'),
       ),
-      body: Card(
-        color: const Color(0xFFBBFFDD),
-        elevation: 5,
-        margin: const EdgeInsets.all(9),
-        child: ListTile(
-          title: Text(
-            cardTitle,
-            style: const TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-          ),
-          subtitle: cardSubtitle,
-        ),
+      // 検出されたコードの数だけリストを作る
+      body: ListView.builder(
+        itemCount: scandata?.barcodes.length ?? 0,
+        itemBuilder: (context, index) {
+
+          // コードから読み取った文字列
+          String codeValue = scandata?.barcodes[index].rawValue ?? 'null';
+          // コードのタイプを示すオブジェクト
+          BarcodeType? codeType = scandata?.barcodes[index].type;
+          // コードのタイプを文字列にする
+          String cardTitle = "[${'$codeType'.split('.').last}]";
+          // 読み取った内容を表示するウィジェット
+          dynamic cardSubtitle = Text(codeValue,
+              style: const TextStyle(fontSize: 23, color: Color(0xFF553311)));
+
+          // タイプがURLである場合
+          if (codeType == BarcodeType.url) {
+            cardTitle = 'どこかのURL';
+            cardSubtitle = InkWell(
+              child: Text(
+                codeValue,
+                style: const TextStyle(
+                  fontSize: 23,
+                  color: Color(0xFF1133DD), // 藍色の文字
+                  decoration: TextDecoration.underline, // 下線
+                  decorationColor: Color(0xFF1133DD), // 下線の色
+                ),
+              ),
+              // 押したらウェブサイトに入る
+              // onTap: () async {
+              //   if (await canLaunchUrlString(codeValue)) {
+              //     await launchUrlString(codeValue);
+              //   }
+              // },
+            );
+          }
+          return Card(
+            elevation: 5,
+            margin: const EdgeInsets.all(9),
+            child: ListTile(
+              title: Text(
+                cardTitle,
+                style:
+                    const TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+              ),
+              subtitle: cardSubtitle,
+            ),
+          );
+        },
       ),
     );
   }
