85c6de28bbaa349d6d79bb3dc40890b29c0dff07
Mitsutan <83488293+Mitsutan@users.noreply.github.com>
2024-03-15 22:30:13 +0900

    Update result.dart
    残骸削除

--- lib/result.dart
+++ lib/result.dart
@@ -9,18 +9,7 @@ class ScanDataWidget extends StatelessWidget {
     super.key,
     this.scandata,
   });
-  // List items = [];
-  // Future<void> getData(String isbn) async {
-  //   var response =
-  //       await http.get(Uri.https('api.openbd.jp', '/v1/get', {'isbn': isbn}));
 
-  //   var jsonResponse = jsonDecode(response.body);
-
-  //   // return jsonResponse[0]; //['onix']['ProductSupply']['SupplyDetail']['Price'][0]['PriceAmount'];
-  //   setState(() {
-  //     items = jsonResponse[0];
-  //   });
-  // }
   Future<Map<String, dynamic>> getData(String isbn) async {
     var response =
         await http.get(Uri.https('api.openbd.jp', '/v1/get', {'isbn': isbn}));
@@ -38,52 +27,7 @@ class ScanDataWidget extends StatelessWidget {
       // 検出されたコードの数だけリストを作る
       body: ListView.builder(
         itemCount: scandata?.barcodes.length ?? 0,
-        // itemBuilder: (context, index) {
-        //   // コードから読み取った文字列
-        //   String codeValue = scandata?.barcodes[index].rawValue ?? 'null';
-        //   getData(codeValue);
-        //   // コードのタイプを示すオブジェクト
-        //   BarcodeType? codeType = scandata?.barcodes[index].type;
-        //   // コードのタイプを文字列にする
-        //   // String cardTitle = "[${'$codeType'.split('.').last}]";
-        //   String cardTitle = items[index]['onix']['DescriptiveDetail']
-        //       ['TitleDetail']['TitleElement']['TitleText']['content'];
-        //   // 読み取った内容を表示するウィジェット
-        //   String cardSubtitle = items[index]['onix']['ProductSupply']
-        //       ['SupplyDetail']['Price'][0]['PriceAmount'];
-        //   // dynamic cardSubtitle = Text(codeValue,
-        //   //     style: const TextStyle(fontSize: 23, color: Color(0xFF553311)));
-        //   // dynamic cardSubtitle = FutureBuilder<String>(
-        //   //   future: getData(codeValue),
-        //   //   builder: (context, snapshot) {
-        //   //     if (snapshot.connectionState == ConnectionState.waiting) {
-        //   //       return const CircularProgressIndicator();
-        //   //     } else if (snapshot.hasError) {
-        //   //       print(snapshot.error);
-        //   //       return Text('エラー: ${snapshot.error}');
-        //   //     } else {
-        //   //       return Text('価格: ${snapshot.data}円',
-        //   //           style: const TextStyle(fontSize: 23, color: Color(0xFF553311)));
-        //   //     }
-        //   //   },
-        //   // );
 
-        //   return Card(
-        //     elevation: 5,
-        //     margin: const EdgeInsets.all(9),
-        //     child: ListTile(
-        //       title: Text(
-        //         cardTitle,
-        //         style:
-        //             const TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-        //       ),
-        //       subtitle: Text(
-        //         cardSubtitle,
-        //         style: const TextStyle(fontSize: 23, color: Color(0xFF553311)),
-        //       ),
-        //     ),
-        //   );
-        // },
         // ListView.builder内
         itemBuilder: (context, index) {
           String codeValue = scandata?.barcodes[index].rawValue ?? 'null';
